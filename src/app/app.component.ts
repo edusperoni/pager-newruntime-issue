475ae849f366fb066eb184dbadf5873320f0c99a
Eduardo Speroni <edusperoni@gmail.com>
2025-02-18 15:43:30 -0300

    feat: add pager

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component, NO_ERRORS_SCHEMA } from '@angular/core';
 import { PageRouterOutlet } from '@nativescript/angular';
+import { PagerModule } from '@triniwiz/nativescript-pager/angular';
 
 @Component({
   selector: 'ns-app',
   templateUrl: './app.component.html',
-  imports: [PageRouterOutlet],
+  imports: [PageRouterOutlet, PagerModule],
   schemas: [NO_ERRORS_SCHEMA],
 })
-export class AppComponent {}
+export class AppComponent {
+  items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+}
